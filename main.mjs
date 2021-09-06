c01d4b273b87c724c5b5e10df1de649f46a94b1c
dodson <matthew.dodson.dc@gmail.com>
2021-09-06 14:18:39 -0400

    Make GameLoop into a class

--- main.mjs
+++ main.mjs
@@ -1,15 +1,43 @@
+// var s = 0;
+//
+// (function loop() {
+//   s++;
+//   console.log('f ', s);
+//   if (s < 5) {
+//     loop();
+//   }
+// })();
+
 import GameLoop from './modules/game-loop.mjs';
 
-let g = new GameLoop(1000);
+let g = new GameLoop();
 
-// g.processInput = function () {
-//   console.log('processInput ', this);
-// }
-// g.render = function () {
-//   console.log('render ', this);
-// }
-// g.update = function () {
-//   console.log('update ', this);
-// }
+g.processInput = function () {
+  // console.log('processInput ', this);
+}
+g.render = function () {
+  // console.log('render ', this);
+}
+g.update = function () {
+  // console.log('UPDATE ', this);
+}
 
-g.runGame()
+// g.runGame();
+
+// function Foo() {}
+//
+// Foo.prototype.sentry = 0;
+//
+// Foo.prototype.fart = function() {
+//   this.sentry += 1;
+//   console.log('PPPPBBBTTT! ', this);
+//   if (this.sentry < 3) {
+//     requestAnimationFrame((a) => {
+//       console.log('F: ', this);
+//       this.fart();
+//     });
+//   }
+// }
+//
+// var x = new Foo();
+// x.fart();
--- modules/game-loop.mjs
+++ modules/game-loop.mjs
@@ -1,40 +1,38 @@
 export default class GameLoop {
-  constructor(updateInterval = 16, prevRealTime = new Date().getTime(),
-              gameClockLag = 0.0) {
-    this.updateInterval = updateInterval; //in ms
-    this.prevRealTime = prevRealTime;
-    this.gameClockLag = gameClockLag; // Delta between game clock and real world clock
-  }
+  updateInterval = 1000;
+  #prevRealTime = new Date().getTime();
+  #gameClockLag = 0.0;
 
   processInput() {
     console.info('[GameLoop processInput] Please provide a method definition for processInput() on this GameLoop instance');
   }
-
   render() {
     console.info('[GameLoop render] Please provide a method definition for render() on this GameLoop instance');
   }
-
   update() {
     console.info('[GameLoop update] Please provide a method definition for update() on this GameLoop instance');
   }
-
   runGame() {
-    // let currentRealTime = new Date().getTime();
-    // let elapsedRealTime = currentRealTime - this.prevRealTime;
-    //
-    // this.prevRealTime = currentRealTime;
-    // this.gameClockLag += elapsedRealTime;
-    //
+    let currentRealTime = new Date().getTime();
+    let elapsedRealTime = currentRealTime - this.#prevRealTime;
+
+    this.#prevRealTime = currentRealTime;
+    this.#gameClockLag += elapsedRealTime;
+
     this.processInput();
-    //
-    // while (this.gameClockLag >= this.updateInterval) {
-    //   this.update();
-    //   this.gameClockLag -= this.updateInterval;
-    // }
-    //
-    // this.render(this.gameClockLag / this.updateInterval);
-    requestAnimationFrame.bind(this, this.runGame);
-    console.log('here');
-    // requestAnimationFrame(this.runGame);
+
+    console.log(this.#gameClockLag);
+
+    while (this.#gameClockLag >= this.updateInterval) {
+      this.update();
+      this.#gameClockLag -= this.updateInterval;
+    }
+
+    this.render(this.#gameClockLag / this.updateInterval);
+
+    requestAnimationFrame.bind(this, GameLoop.runGame);
+    requestAnimationFrame((t) => {
+      this.runGame();
+    });
   }
 }
