ec528610fbdfd2c1688ec2cef84a032c497e7ba0
dodson <matthew.dodson.dc@gmail.com>
2021-09-06 21:28:50 -0400

    WIP Add new Classes for game elements

--- main.mjs
+++ main.mjs
@@ -1,8 +1,33 @@
-import GameLoop from './modules/game-loop.mjs';
-import GameController from './modules/game-controller.mjs';
-import Observer from './modules/observer.mjs';
+import Game from './modules/game.mjs';
+import GameBoard from './modules/game-board.mjs';
+import GameField from './modules/game-field.mjs';
+import Player from './modules/player.mjs';
 
-
+// import GameLoop from './modules/game-loop.mjs';
+// import GameController from './modules/game-controller.mjs';
+// import Observer from './modules/observer.mjs';
+//
+const GAME_CONFIG = {
+  ref: 'game-board',
+  hostEl: document.querySelector('#tetrish-game'),
+  cols: 12,
+  rows: 20,
+  scale: 20,
+  get width() { return this.cols * this.scale },
+  get height() { return this.rows * this.scale },
+  bgc: '#00000066',
+}
+let gameBoard =
+  new GameBoard(
+    GAME_CONFIG.ref,
+    GAME_CONFIG.hostEl,
+    GAME_CONFIG.width,
+    GAME_CONFIG.height,
+    GAME_CONFIG.scale,
+  );
+//
+//
+// let game = new Game(gameBoard, gameField, player);
 //
 //
 // let gameController = new GameController();
@@ -47,3 +72,8 @@ import Observer from './modules/observer.mjs';
 // }
 //
 // // gameLoop.runGame();
+
+// -------------------------
+// If we get this far, then its safe to remove the noscript & feature-check
+// messages in the DOM. We're going to be forcing a render tree update anyway.
+document.querySelector('#preflight-err').remove();
